9b89c82497277f1dac92d069ad9afb0825942572
NI22KHIL99 <89829825+NI22KHIL99@users.noreply.github.com>
2022-08-05 23:52:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import NewsPage from './components/NewsPage';
+
+const App = () => {
+  return <NewsPage></NewsPage>
+}
+
+
+
+export default App;
---
+++ src/components/NewsCard.js
@@ -0,0 +1,20 @@
+const NewsCard = ({article}) => {
+    if(!article.title)return null;
+    return(
+        <div className="news-card">
+            <h3>{article.title}</h3>
+            <a href = {article.url}>Read More</a>
+        </div>
+    )
+
+};
+
+
+
+
+
+
+
+
+
+export default NewsCard;
